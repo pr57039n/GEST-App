2e34d9b1675f3f9f82689184b8cb966535686ec0
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-12-11 22:09:29 -0500

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,20 @@
-describe('Test', () => {
+describe('Container accessible', () => {
   it.only('Verify Title', () => {
     cy.visit('http://172.17.0.1:8000')
     cy.title().should('eq','GEST')
+  })
+  it.only('Has incorrect details', () => {
+    cy.visit('http://172.17.0.1:8000')
+    cy.get('form');
+    cy.get('#form2Example17')
+    .type('admin@gmail.com')
+    .should('have.value', 'admin@gmail.com');
+    cy.get('#form2Example27')
+    .type('badpassword')
+    .should('have.value', 'badpassword');
+    cy.get('form').submit()
+    cy.get('.alert')
+    .should('be.visible')
+    .and('contain.text', 'Invalid details')
   })
 })
