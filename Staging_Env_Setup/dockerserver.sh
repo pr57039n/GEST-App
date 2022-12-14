3d459c92c044f3e1295928d1cea9e403e7d9edca
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-12-13 21:39:15 -0500

    Update dockerserver.sh

--- Staging_Env_Setup/dockerserver.sh
+++ Staging_Env_Setup/dockerserver.sh
@@ -4,7 +4,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo apt update
 sudo apt -y install default-jre
-curl -fsSL https://share.wildbook.me/4PNafRIcXKRT2K5D.tar --output cypress.tar
+curl -fsSL https://share.wildbook.me/lmRhIVUdqSwlGfhM.tar --output cypress.tar
 sudo apt -y install firefox
 sudo tar -xf cypress.tar
 sudo docker pull bikigrg/prod_env_setup_nginx:latest
@@ -15,4 +15,4 @@ sudo docker run -it \
 -v $PWD:/e2e \
 -w /e2e \
 -e CYPRESS_baseUrl=http://172.17.0.1:80 \
-cypress/included:12.0.2 --browser firefox
+cypress/included:12.0.2 --browser firefox > cylog_$(date +%d-%m-%y);
