90034e73ccfedffc77a355cad37accd53e34eea2
IshtaarD <101521350+IshtaarD@users.noreply.github.com>
2022-12-13 23:45:03 -0500

    changed image repo names

--- Staging_Env_Setup/dockerserver.sh
+++ Staging_Env_Setup/dockerserver.sh
@@ -9,8 +9,8 @@ sudo apt -y install firefox
 sudo tar -xf cypress.tar
 sudo docker pull bikigrg/nginx_proxy:latest
 sudo docker pull bikigrg/gest_app:latest
-sudo docker run -d -p 80:80 bikigrg/prod_env_setup_nginx:latest
-sudo docker run -d -p 80:80 bikigrg/prod_env_setup_web:latest
+sudo docker run -d -p 80:80 bikigrg/nginx_proxy:latest
+sudo docker run -d -p 80:80 bikigrg/gest_app:latest
 sudo docker run -it \
 -v $PWD:/e2e \
 -w /e2e \
