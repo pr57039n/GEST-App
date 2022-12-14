05c04b165bb22a256fb061307a56e0ee685262d9
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-12-13 22:46:36 -0500

    Update dockerserver.sh

--- Staging_Env_Setup/dockerserver.sh
+++ Staging_Env_Setup/dockerserver.sh
@@ -9,7 +9,7 @@ sudo apt -y install firefox
 sudo tar -xf cypress.tar
 sudo docker pull bikigrg/prod_env_setup_nginx:latest
 sudo docker pull bikigrg/prod_env_setup_web:latest
-sudo docker run -d -p 8000:80 bikigrg/prod_env_setup_nginx:latest
+sudo docker run -d -p 80:80 bikigrg/prod_env_setup_nginx:latest
 sudo docker run -d -p 80:8000 bikigrg/prod_env_setup_web:latest
 sudo docker run -it \
 -v $PWD:/e2e \
